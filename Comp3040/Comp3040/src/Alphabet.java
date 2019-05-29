cbf81101a6e6af5cffd14796d702aca2aacc3de0
benguler <50894078+benguler@users.noreply.github.com>
2019-05-28 23:49:28 -0400

    Completed task #8

---
+++ Comp3040/Comp3040/src/Alphabet.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Alphabet {
+	
+	private ArrayList<Character> alphabet = new ArrayList<Character>(); 
+	
+	public Alphabet(){
+		
+	}
+	
+	public Alphabet(ArrayList<Character> alphabet){
+		this.alphabet = alphabet;
+		
+	}
+	
+	public int size(){
+		return alphabet.size();
+		
+	}
+	
+	public void set(int i, Character character){
+		alphabet.set(i, character);
+		
+	}
+	
+	public Character get(int i){
+		return alphabet.get(i);
+		
+	}
+	
+	public int findIndex(Character character){
+		return alphabet.indexOf(character);
+	}
+	
+}
