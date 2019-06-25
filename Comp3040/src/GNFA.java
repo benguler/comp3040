3639529dfdaefb94af71843830326f09717f8ed0
benguler <50894078+benguler@users.noreply.github.com>
2019-06-25 06:04:40 -0400

    Completed Task #51

---
+++ Comp3040/src/GNFA.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class GNFA {
+	public GNFA(ArrayList<State> states, Alphabet alphabet, State startState, ArrayList<RegEx> nextRegs, State acceptingState, Character epsilon){
+		
+	}
+	
+}
